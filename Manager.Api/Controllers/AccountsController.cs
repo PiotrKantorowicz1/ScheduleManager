51776cb18e1544b52e59222d5fbdbaa6fbfce9e8
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-03-12 23:56:59 +0100

    Fixed Startup class and nuget packages

---
+++ Manager.Api/Controllers/AccountsController.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Manager.Core.Models;
+using Manager.Struct.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manager.Api.Controllers
+{
+    [Route("api/[Controller]")]
+    public class AccountsController : Controller
+    {
+        private readonly IAccountService _accounteService;
+        private readonly IRefreshTokenService _refreshTokenService;
+
+        public AccountsController(IAccountService accounteService,
+            IRefreshTokenService refreshTokenService)
+        {
+            _accounteService = accounteService;
+            _refreshTokenService = refreshTokenService;
+        }
+
+        [HttpPost("sign-in/{email}/{password}")]
+        public async Task<IActionResult> SignIn(string email, string password)
+            => Ok(await _accounteService.SignInAsync(email, password));
+    }
+}
