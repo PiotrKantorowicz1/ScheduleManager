30b14f40ae25326fc38933a22593bae4acafd228
Piotr Kantorowicz <piotrkantorowicz@gmail.com>
2018-03-17 01:38:23 +0100

    RegisterCommandsInAutofacAndAddTaskHandlers

---
+++ Manager.Struct/IoC/Modules/CommandModule.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Autofac;
+using Manager.Struct.Commands;
+
+namespace Manager.Struct.IoC.Modules
+{
+    public class CommandModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assembly = typeof(CommandModule)
+                .GetTypeInfo()
+                .Assembly;
+
+            builder.RegisterAssemblyTypes(assembly)
+                .AsClosedTypesOf(typeof(ICommandHandler<>))
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<CommandDispatcher>()
+                .As<ICommandDispatcher>()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
